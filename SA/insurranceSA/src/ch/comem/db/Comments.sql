f9457a4e86313bc56a653a5d3c92496938bdb5e2
Manu Bezençon <Groupe GoF>
2015-03-31 10:49:28 +0200

    BD Insurrance
    - Création tables avec fichiers sql par table
    - Création des foreign key avec fichier sql unique

---
+++ SA/insurranceSA/src/ch/comem/db/Comments.sql
@@ -0,0 +1,20 @@
+    CREATE TABLE Comments (
+    ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 4001, INCREMENT BY 1),
+    TEXT VARCHAR(100) NOT NULL,
+    WORKER_ID INTEGER NOT NULL,
+    CERTIFICATE_ID INTEGER NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+INSERT INTO Comments (TEXT, WORKER_ID, CERTIFICATE_ID)
+    VALUES
+    ('Commentaire numéro 1', 3001, 1001),
+    ('Commentaire numéro 2', 3002, 1002),
+    ('Commentaire numéro 3', 3003, 1003),
+    ('Commentaire numéro 4', 3004, 1004),
+    ('Commentaire numéro 5', 3005, 1005);
+
+/*ALTER TABLE Peoples
+ADD FOREIGN KEY (AUTO_ID) REFERENCES Autos(SERIAL_NUMBER);*/
+
+
