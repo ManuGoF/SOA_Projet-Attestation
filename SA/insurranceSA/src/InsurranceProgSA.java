5f1c4236bd2dcd096839c130f4e26012cdf69e39
Cedric <cliengme@gmail.com>
2015-03-31 14:10:48 +0200

    sql

--- SA/insurranceSA/src/InsurranceProgSA.java
+++ SA/insurranceSA/src/InsurranceProgSA.java
@@ -1,3 +1,6 @@
+
+import ch.comem.model.WorkerModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +17,7 @@ public class InsurranceProgSA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        WorkerModel worker = new WorkerModel("Liengme", "Cedric", "cliengme@gmail.com", "garage");
     }
     
 }
--- SA/insurranceSA/src/ch/comem/db/Workers.sql
+++ SA/insurranceSA/src/ch/comem/db/Workers.sql
@@ -5,16 +5,15 @@
     EMAIL VARCHAR(80) NOT NULL,
     POSITION VARCHAR(20) NOT NULL,
     CHECK (POSITION IN ('garage', 'agence', 'siege', 'direction')),
-    CERTIFICATE_NUMBER VARCHAR(50) NOT NULL, 
     PRIMARY KEY (ID)
 );
 
-INSERT INTO Workers (LASTNAME, FIRSTNAME, EMAIL, POSITION, CERTIFICATE_NUMBER)
+INSERT INTO Workers (LASTNAME, FIRSTNAME, EMAIL, POSITION)
     VALUES
-    ('Mavic', 'Arnold', 'mavic.arnold@gmail.com', 'garage', 'ASS10 5000 1001'),
-    ('Bateau', 'Jean', 'bateau.jean@gmail.com', 'garage', 'ASS10 5000 2002'),
-    ('Rochat', 'Loic', 'rochat.loic@gmail.com', 'garage', 'ASS10 5000 3003'),
-    ('Mayer', 'Justin', 'mayer.justin@gmail.com', 'garage', 'ASS10 5000 4004'),
-    ('Wenger', 'Eric', 'wenger.eric@gmail.com', 'garage', 'ASS10 5000 5005');
+    ('Mavic', 'Arnold', 'mavic.arnold@gmail.com', 'garage'),
+    ('Bateau', 'Jean', 'bateau.jean@gmail.com', 'garage'),
+    ('Rochat', 'Loic', 'rochat.loic@gmail.com', 'garage'),
+    ('Mayer', 'Justin', 'mayer.justin@gmail.com', 'garage'),
+    ('Wenger', 'Eric', 'wenger.eric@gmail.com', 'garage');
 
 
