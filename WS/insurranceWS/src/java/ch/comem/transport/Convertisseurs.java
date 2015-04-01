105cbe849f48290dbd332e3ae7c63e9a1b9bfc35
Manu Bezençon <Groupe GoF>
2015-04-01 11:02:27 +0200

    Convertisseur
    Ajout des convertisseurs pour car et client

--- WS/insurranceWS/src/java/ch/comem/transport/Convertisseurs.java
+++ WS/insurranceWS/src/java/ch/comem/transport/Convertisseurs.java
@@ -7,6 +7,7 @@ package ch.comem.transport;
 
 import ch.comem.model.CarModel;
 import ch.comem.model.CertificateModel;
+import ch.comem.model.ClientModel;
 import ch.comem.model.WorkerModel;
 
 /**
@@ -112,6 +113,98 @@ public class Convertisseurs {
 
         }
         return ct;
+    }
+    
+    /**
+     * Permet de convertir un client de type ClientModel en type ClientTransport.
+     * Cela permet de convertir le client du SA en client transportable (sur le Web).
+     * Si l'objet passé en paramètre est null ou si l'id du client < 0, rien n'est retourné.
+     * @param id_client de type Integer correspondant à l'id du client
+     * @param client de type ClientModel correspondant à un client du SA.
+     * @return ct de type ClientTransport correspondant à un client "transportable".
+     */
+    public static ClientTransport clientToClientTransport (int id_client, ClientModel client) {
+        ClientTransport ct = null;
+        if (id_client > 0 || client != null) {
+            ct = new ClientTransport();
+            ct.setId(id_client);
+            ct.setLastname(client.lastname);
+            ct.setFirstname(client.firstname);
+            ct.setEmail(client.email);
+            ct.setBirthday(client.birthday);
+            ct.setLicence_type(client.licence_type);
+            ct.setLicence_date(client.licence_date);
+        }
+        return ct;
+    }
+    
+    /**
+     * Permet de convertir un client de type ClientTransport en type ClientModel.
+     * Cela permet de convertir le client du WS transportable (sur le Web) en client SA utilisable par le SA.
+     * Si l'objet passé en paramètre est null, rien n'est retourné. (Ou null).
+     * @param ct de type ClientTransport correspondant à un client transportable (sur le web)
+     * @return client de type ClientModel correspondant à un client du SA.
+     */
+    public static ClientModel clientTransportToClient (ClientTransport ct) {
+        ClientModel client = null;
+        if (ct != null) {
+            client = new ClientModel(
+                ct.getLastname(),
+                ct.getFirstname(),
+                ct.getEmail(),
+                ct.getBirthday(),
+                ct.getLicence_type(),
+                ct.getLicence_date()
+            );
+        }
+        return client;   
+    }
+    
+    /**
+     * Permet de convertir un véhicule de type CarModel en type CarTransport.
+     * Cela permet de convertir le véhicule du SA en véhicule transportable (sur le Web).
+     * Si l'objet passé en paramètre est null ou l'id < 0, rien n'est retourné.
+     * @param id_client de type Integer correspondant à l'id du client à qui apprtient le véhicule.
+     * @param car de type CarModel correspondant à un véhicule du SA.
+     * @return cart de type CarTransport correspondant à un véhicule "transportable".
+     */
+    public static CarTransport carToCarTransport (int id_client, CarModel car) {
+        CarTransport cart = null;
+        if (car != null) {
+            cart = new CarTransport();
+            cart.setSerial_number(car.serial_number);
+            cart.setBrand(car.brand);
+            cart.setModel(car.model);
+            cart.setType(car.type);
+            cart.setColor(car.color);
+            cart.setPrice(car.price);
+            cart.setClient(Convertisseurs.clientToClientTransport(id_client, car.client));
+        }
+         
+        return cart;
+    }
+    
+    /**
+     * Permet de convertir un véhicule de type CarTransport en type CarModel.
+     * Cela permet de convertir le véhicule du WS transportable (sur le Web) en véhicule SA utilisable par le SA.
+     * Si l'objet passé en paramètre est null, rien n'est retourné. (Ou null).
+     * @param cart de type CarTransport correspondant à un véhicule transportable (sur le web)
+     * @return car de type CarModel correspondant à un véhicule du SA.
+     */
+    public static CarModel carTransportToCar (CarTransport cart) {
+        CarModel car = null;
+        if (cart != null) {
+            car = new CarModel(
+                cart.getSerial_number(),
+                cart.getBrand(),
+                cart.getModel(),
+                cart.getType(),
+                cart.getColor(),
+                cart.getPrice(),
+                Convertisseurs.clientTransportToClient(cart.getClient())
+            );
+        }
+        return car;   
     }
     
 }
