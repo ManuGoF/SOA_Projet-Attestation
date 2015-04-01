c2a17a1596130020bf7d2008fb682d04cdfa629e
Manu Bezençon <Groupe GoF>
2015-04-01 11:42:37 +0200

    WS MANU
    WS de Client OK

--- WS/insurranceWS/src/java/ch/comem/ws/inssuranceManuWS.java
+++ WS/insurranceWS/src/java/ch/comem/ws/inssuranceManuWS.java
@@ -11,6 +11,9 @@ import ch.comem.model.Response;
 import ch.comem.transport.ClientTransport;
 import ch.comem.transport.Convertisseurs;
 import ch.comem.transport.ResponseTransport;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -34,6 +37,8 @@ public class inssuranceManuWS {
 
     /**
      * Web service operation
+     * @param client
+     * @return 
      */
     @WebMethod(operationName = "createClient")
     public ResponseTransport createClient(@WebParam(name = "client") ClientTransport client) {
@@ -41,6 +46,53 @@ public class inssuranceManuWS {
         if (client != null) {
             ClientModel clientSA = Convertisseurs.clientTransportToClient(client);
             Response responseSA = ClientController.createClient(clientSA);
+            responseWS = Convertisseurs.responseToResponseTransport(responseSA);
+        }
+        return responseWS;
+    }
+
+    /**
+     * Web service operation
+     * @param client_id
+     * @return 
+     */
+    @WebMethod(operationName = "readClient")
+    public ClientTransport readClient(@WebParam(name = "client_id") int client_id) {
+        ClientTransport clientWS = new ClientTransport();
+        if (client_id >= 0) {
+            ClientModel clientSA = ClientController.readClient(client_id);
+            clientWS = Convertisseurs.clientToClientTransport(client_id, clientSA);
+        }
+        return clientWS;
+    }
+
+    /**
+     * Web service operation
+     * @return 
+     */
+    @WebMethod(operationName = "readAllClients")
+    public List<ClientTransport> readAllClients() {
+        List<ClientTransport> clientsListWS = new ArrayList<>();
+        Map<Integer, ClientModel> clients = ClientController.readAllClients();
+        
+        for (Map.Entry<Integer, ClientModel> entry: clients.entrySet()) {
+            ClientTransport clientWS = Convertisseurs.clientToClientTransport(entry.getKey(),entry.getValue());
+            clientsListWS.add(clientWS);
+        }
+
+        return clientsListWS;
+    }
+
+    /**
+     * Web service operation
+     * @param client_id
+     * @return 
+     */
+    @WebMethod(operationName = "deleteClient")
+    public ResponseTransport deleteClient(@WebParam(name = "client_id") int client_id) {
+        ResponseTransport responseWS = null;
+        if (client_id > 0) {
+            Response responseSA = ClientController.deleteClient(client_id);
             responseWS = Convertisseurs.responseToResponseTransport(responseSA);
         }
         return responseWS;
